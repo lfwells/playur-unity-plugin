1f362059979285c9cf4575e3495654fdfe57378b
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-08-15 01:31:22 +1000

    new ui for configuration

--- Editor/PlayURSettingsProvider.cs
+++ Editor/PlayURSettingsProvider.cs
@@ -11,10 +11,37 @@ namespace PlayUR
         /// <summary>Current PlayUR settings</summary>
         private SerializedObject playurSettings;
         private SerializedProperty gameIdProperty;
+        private SerializedProperty standardSessionTracking;
+        private SerializedProperty useSpecificExperimentForMobileBuild;
+        private SerializedProperty mobileExperiment;
+        private SerializedProperty useSpecificExperimentForDesktopBuild;
+        private SerializedProperty desktopExperiment;
+        private SerializedProperty mTurkCompletionMessage;
+        private SerializedProperty forceToUseSpecificExperiment;
+        private SerializedProperty experimentToTestInEditor;
+        private SerializedProperty forceToUseSpecificGroup;
+        private SerializedProperty groupToTestInEditor;
+        private SerializedProperty forceMTurkIDInEditor;
+        private SerializedProperty defaultHighScoreTablePrefab;
+        private SerializedProperty defaultPopupPrefab;
+        private SerializedProperty defaultSurveyPopupPrefab;
+        private SerializedProperty defaultSurveyRowPrefab;
+        private SerializedProperty mTurkLogo;
         private SerializedProperty logLevelProperty;
 
         private SerializedObject playurClientSecretSettings;
         private SerializedProperty clientSecretProperty;
+
+        private bool _foldout_checks = true;
+
+        private bool _foldout_config = true;
+        private bool _foldout_general = false;
+        private bool _foldout_mturk = false;
+        private bool _foldout_editorSettings = false;
+        private bool _foldout_mobileDesktopSettings = false;
+        private bool _foldout_prefabs = false;
+
+        private bool _foldout_enums = true;
 
         private bool _foldout_parameters = false;
         private bool _foldout_experiments = false;
@@ -40,10 +67,33 @@ namespace PlayUR
             public static GUIContent gameId = new GUIContent("Game ID");
             public static GUIContent clientSecret = new GUIContent("Client Secret");
 
-            public static GUIContent logging = new GUIContent("Logging");
+            public static GUIContent generalSettings = new GUIContent("General Settings");
+            public static GUIContent standardSessionTracking = new GUIContent("Use Standard Session Tracking");
             public static GUIContent logLevel = new GUIContent("Log Level");
 
-            public static GUIContent enums = new GUIContent("PlayUR Configuration");
+            public static GUIContent mTurk = new GUIContent("MTurk Settings");
+            public static GUIContent mTurkCompletionMessage = new GUIContent("MTurk Completion Message");
+            public static GUIContent mTurkSprite = new GUIContent("MTurk Sprite");
+            public static GUIContent mTurkForceID = new GUIContent("Force MTurk ID in Editor");
+
+
+            public static GUIContent editorSettings = new GUIContent("Editor Settings");
+            public static GUIContent forceExperiment = new GUIContent("Force Experiment in Editor");
+            public static GUIContent experiment = new GUIContent("Experiment");
+            public static GUIContent forceExperimentGroup = new GUIContent("Force Experiment Group in Editor");
+            public static GUIContent experimentGroup = new GUIContent("Experiment Group");
+
+            public static GUIContent mobileAndDesktop = new GUIContent("Mobile and Desktop Settings");
+            public static GUIContent mobileExpForce = new GUIContent("Force Experiment on Mobile");
+            public static GUIContent desktopExpForce = new GUIContent("Force Experiment on Desktop");
+
+            public static GUIContent prefabsSettings = new GUIContent("Prefab Settings");
+            public static GUIContent prefabHighScoreTable = new GUIContent("High Score Table");
+            public static GUIContent prefabPopup = new GUIContent("Popup");
+            public static GUIContent prefabSurveyPopup = new GUIContent("Survey Popup");
+            public static GUIContent prefabSurveyRow = new GUIContent("Survey Row");
+
+            public static GUIContent enums = new GUIContent("Debug PlayUR Configuration");
             public static GUIContent generateEnums = new GUIContent("Generate Enums");
 
             public static GUIContent parameters = new GUIContent("Parameters");
@@ -77,6 +127,22 @@ namespace PlayUR
             // Load the default settings (or create a new one) when the settings window is first clicked.
             playurSettings = PlayURSettings.GetSerializedSettings();
             gameIdProperty = playurSettings.FindProperty("gameId");
+            standardSessionTracking = playurSettings.FindProperty("standardSessionTracking");
+            useSpecificExperimentForMobileBuild = playurSettings.FindProperty("useSpecificExperimentForMobileBuild");
+            mobileExperiment = playurSettings.FindProperty("playurSettings");
+            useSpecificExperimentForDesktopBuild = playurSettings.FindProperty("useSpecificExperimentForDesktopBuild");
+            desktopExperiment = playurSettings.FindProperty("desktopExperiment");
+            mTurkCompletionMessage = playurSettings.FindProperty("mTurkCompletionMessage");
+            forceToUseSpecificExperiment = playurSettings.FindProperty("forceToUseSpecificExperiment");
+            experimentToTestInEditor = playurSettings.FindProperty("experimentToTestInEditor");
+            forceToUseSpecificGroup = playurSettings.FindProperty("forceToUseSpecificGroup");
+            groupToTestInEditor = playurSettings.FindProperty("groupToTestInEditor");
+            defaultHighScoreTablePrefab = playurSettings.FindProperty("defaultHighScoreTablePrefab");
+            defaultPopupPrefab = playurSettings.FindProperty("defaultPopupPrefab");
+            defaultSurveyPopupPrefab = playurSettings.FindProperty("defaultSurveyPopupPrefab");
+            defaultSurveyRowPrefab = playurSettings.FindProperty("defaultSurveyRowPrefab");
+            forceMTurkIDInEditor = playurSettings.FindProperty("forceMTurkIDInEditor");
+            mTurkLogo = playurSettings.FindProperty("mTurkLogo");
             logLevelProperty = playurSettings.FindProperty("minimumLogLevelToStore");
 
             playurClientSecretSettings = PlayURClientSecretSettings.GetSerializedSettings();
@@ -85,6 +151,9 @@ namespace PlayUR
 
         public override void OnGUI(string searchContext)
         {
+            var originalLabelWidth = EditorGUIUtility.labelWidth;
+            EditorGUIUtility.labelWidth = 250;
+
             var redStyle = new GUIStyle(EditorStyles.label);
             redStyle.normal.textColor = Color.red;
             var greenStyle = new GUIStyle(EditorStyles.label);
@@ -95,91 +164,181 @@ namespace PlayUR
             // Set-Up Checks
             EditorGUI.indentLevel = 0;
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
-            
-            GUILayout.Label(Labels.setUpChecks, EditorStyles.boldLabel);
-            EditorGUI.indentLevel = 1;
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(Labels.gameIDSet);
-            if (gameIdProperty.intValue > 0)
-                EditorGUILayout.LabelField(Labels.done, greenStyle);
-            else
-                EditorGUILayout.LabelField(Labels.fixBelow, redStyle);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(Labels.clientSecret);
-            if (!string.IsNullOrEmpty(clientSecretProperty.stringValue))
-                EditorGUILayout.LabelField(Labels.done, greenStyle);
-            else
-                EditorGUILayout.LabelField(Labels.fixBelow, redStyle);
-            EditorGUILayout.EndHorizontal();
-
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(Labels.enumsGenerated);
-            if (System.Enum.GetValues(typeof(Experiment)).Length > 0)
-                EditorGUILayout.LabelField(Labels.done, greenStyle);
-            else
+            _foldout_checks = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_checks, Labels.setUpChecks);
+            if (_foldout_checks)
             {
-                if (GUILayout.Button(Labels.fix, redButton))
+                EditorGUI.indentLevel = 1;
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel(Labels.gameIDSet);
+                if (gameIdProperty.intValue > 0)
+                    EditorGUILayout.LabelField(Labels.done, greenStyle);
+                else
+                    EditorGUILayout.LabelField(Labels.fixBelow, redStyle);
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel(Labels.clientSecret);
+                if (!string.IsNullOrEmpty(clientSecretProperty.stringValue))
+                    EditorGUILayout.LabelField(Labels.done, greenStyle);
+                else
+                    EditorGUILayout.LabelField(Labels.fixBelow, redStyle);
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel(Labels.enumsGenerated);
+                if (System.Enum.GetValues(typeof(Experiment)).Length > 0)
+                    EditorGUILayout.LabelField(Labels.done, greenStyle);
+                else
                 {
-                    PlayURPluginEditor.GenerateEnum();
+                    if (GUILayout.Button(Labels.fix, redButton))
+                    {
+                        PlayURPluginEditor.GenerateEnum();
+                    }
                 }
-            }
-            EditorGUILayout.EndHorizontal();
+                EditorGUILayout.EndHorizontal();
 
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PrefixLabel(Labels.buildSettingsSet);
-            var scenes = new List<EditorBuildSettingsScene>();
-            scenes.AddRange(EditorBuildSettings.scenes);
-            ;
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.PrefixLabel(Labels.buildSettingsSet);
+                var scenes = new List<EditorBuildSettingsScene>();
+                scenes.AddRange(EditorBuildSettings.scenes);
 
-           
-            if (scenes.Where((scene) => scene.path.Contains("PlayURLogin")).Count() == 1)
-                EditorGUILayout.LabelField(Labels.done, greenStyle);
-            else
-            {
-                if (GUILayout.Button(Labels.fix))
+                if (scenes.Where((scene) => scene.path.Contains("PlayURLogin")).Count() == 1)
+                    EditorGUILayout.LabelField(Labels.done, greenStyle);
+                else
                 {
-                    PlayURPluginEditor.SetSceneBuildSettings();
+                    if (GUILayout.Button(Labels.fix))
+                    {
+                        PlayURPluginEditor.SetSceneBuildSettings();
+                    }
                 }
+                EditorGUILayout.EndHorizontal();
             }
-            EditorGUILayout.EndHorizontal();
-
+            EditorGUILayout.EndFoldoutHeaderGroup();
             EditorGUI.indentLevel = 0;
             EditorGUILayout.EndVertical();
+
 
 
             // Game Configuration
             EditorGUI.indentLevel = 0;
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_config = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_config, Labels.gameConfiguration);
+            if (_foldout_config)
             {
-                GUILayout.Label(Labels.gameConfiguration, EditorStyles.boldLabel);
                 EditorGUI.indentLevel = 1;
                 EditorGUILayout.PropertyField(gameIdProperty, Labels.gameId);
                 EditorGUILayout.PropertyField(clientSecretProperty, Labels.clientSecret);
             }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
             EditorGUILayout.EndVertical();
 
             // Settings
             EditorGUI.indentLevel = 0;
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_general = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_general, Labels.generalSettings);
+            if (_foldout_general)
             {
-                GUILayout.Label(Labels.logging, EditorStyles.boldLabel);
                 EditorGUI.indentLevel = 1;
+                EditorGUILayout.PropertyField(standardSessionTracking, Labels.standardSessionTracking);
                 EditorGUILayout.PropertyField(logLevelProperty, Labels.logLevel);
             }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
             EditorGUILayout.EndVertical();
 
-            // Actions
+
+            // Editor Settings
             EditorGUI.indentLevel = 0;
             EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_editorSettings = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_editorSettings, Labels.editorSettings);
+            if (_foldout_editorSettings)
             {
-                GUILayout.Label(Labels.enums, EditorStyles.boldLabel);
+                EditorGUI.indentLevel = 1;
+                EditorGUILayout.PropertyField(forceToUseSpecificExperiment, Labels.forceExperiment);
+                if (forceToUseSpecificExperiment.boolValue)
+                {
+                    EditorGUILayout.PropertyField(experimentToTestInEditor, Labels.experiment);
+                }
+                EditorGUILayout.PropertyField(forceToUseSpecificGroup, Labels.forceExperimentGroup);
+                if (forceToUseSpecificGroup.boolValue)
+                {
+                    EditorGUILayout.PropertyField(groupToTestInEditor, Labels.experimentGroup);
+                }
+
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.EndVertical();
+
+            // Mobile and Desktop Settings
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_mobileDesktopSettings = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_mobileDesktopSettings, Labels.mobileAndDesktop);
+            if (_foldout_mobileDesktopSettings)
+            {
+                EditorGUI.indentLevel = 1;
+                EditorGUILayout.PropertyField(useSpecificExperimentForMobileBuild, Labels.mobileExpForce);
+                if (useSpecificExperimentForMobileBuild.boolValue)
+                {
+                    EditorGUILayout.PropertyField(mobileExperiment, Labels.experiment);
+                }
+                EditorGUILayout.PropertyField(useSpecificExperimentForDesktopBuild, Labels.desktopExpForce);
+                if (useSpecificExperimentForDesktopBuild.boolValue)
+                {
+                    EditorGUILayout.PropertyField(desktopExperiment, Labels.experimentGroup);
+                }
+
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.EndVertical();
+
+
+            // Prefabs
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_prefabs = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_prefabs, Labels.prefabsSettings);
+            if (_foldout_prefabs)
+            {
+                EditorGUI.indentLevel = 1;
+                EditorGUILayout.PropertyField(defaultHighScoreTablePrefab, Labels.prefabHighScoreTable);
+                EditorGUILayout.PropertyField(defaultPopupPrefab, Labels.prefabPopup);
+                EditorGUILayout.PropertyField(defaultSurveyPopupPrefab, Labels.prefabSurveyPopup);
+                EditorGUILayout.PropertyField(defaultSurveyRowPrefab, Labels.prefabSurveyRow);
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.EndVertical();
+
+
+            // MTurk
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_mturk = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_mturk, Labels.mTurk);
+            if (_foldout_mturk)
+            {
+                EditorGUI.indentLevel = 1;
+                EditorGUILayout.PropertyField(mTurkCompletionMessage, Labels.mTurkCompletionMessage);
+                EditorGUILayout.PropertyField(forceMTurkIDInEditor, Labels.mTurkForceID);
+                EditorGUILayout.PropertyField(mTurkLogo, Labels.mTurkSprite);
+            }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.EndVertical();
+
+
+
+            // ActionsEditorGUI.indentLevel = 0;
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            _foldout_enums = EditorGUILayout.BeginFoldoutHeaderGroup(_foldout_enums, Labels.enums);
+            if (_foldout_enums)
+            {
                 EditorGUI.indentLevel = 1;
                 if (GUILayout.Button(Labels.generateEnums, EditorStyles.miniButton))
                 {
-                    PlayUR.Editor.PlayURPluginEditor.GenerateEnum();
+                    PlayURPluginEditor.GenerateEnum();
                     _parameters = null;
                 }
 
@@ -189,7 +348,11 @@ namespace PlayUR
                 StringListFoldout(ref _foldout_parameters, GetPlayURParameters(), Labels.parameters);
                 EnumNameFoldout<PlayUR.AnalyticsColumn>(ref _foldout_analytics, Labels.analyticColumns);
             }
+            EditorGUILayout.EndFoldoutHeaderGroup();
+            EditorGUI.indentLevel = 0;
             EditorGUILayout.EndVertical();
+
+            EditorGUIUtility.labelWidth = originalLabelWidth;
 
             playurSettings.ApplyModifiedProperties();
             playurClientSecretSettings.ApplyModifiedProperties();
@@ -212,15 +375,18 @@ namespace PlayUR
 
         private void StringListFoldout(ref bool foldout, string[] values, GUIContent label)
         {
-            foldout = EditorGUILayout.BeginFoldoutHeaderGroup(foldout, label + " (" + values.Length + ")");
+            foldout = EditorGUILayout.Foldout(foldout, label + " (" + values.Length + ")");
             if (foldout)
             {
+                EditorGUI.indentLevel = 2;
+                EditorGUILayout.BeginVertical();
                 foreach (var name in values)
                 {
                     EditorGUILayout.SelectableLabel(name, EditorStyles.wordWrappedMiniLabel, GUILayout.MaxHeight(15));
                 }
+                EditorGUILayout.EndVertical();
+                EditorGUI.indentLevel = 1;
             }
-            EditorGUILayout.EndFoldoutHeaderGroup();
         }
 
     }
--- Runtime/Core/PlayURSettings.cs
+++ Runtime/Core/PlayURSettings.cs
@@ -11,17 +11,13 @@ namespace PlayUR
 
         [SerializeField]
         private int gameId;
-
         public int GameID => gameId;
 
-        [Header("Options")]
         /// <summary>
         /// this automagically creates a session handler (using <see cref="PlayURSessionTracker"/>), otherwise if false need to manually called StartSession etc
         /// </summary>
         public bool standardSessionTracking = true;
 
-
-        [Header("Standalone -- Android and iOS (Note these get reset on Plugin Update!)")]
         /// <summary>
         /// Override the PlayUR Platform's automatic choosing of an experiment on mobile builds by forcing the use of <see cref="mobileExperiment"/>
         /// </summary>
@@ -32,8 +28,6 @@ namespace PlayUR
         /// </summary>
         public Experiment mobileExperiment;
 
-
-        [Header("Standalone -- Windows, MacOS, Linux (Note these get reset on Plugin Update!)")]
         /// <summary>
         /// Override the PlayUR Platform's automatic choosing of an experiment on desktop builds by forcing the use of <see cref="desktopExperiment"/>
         /// </summary>
@@ -43,7 +37,6 @@ namespace PlayUR
         /// The experiment to choose if <see cref="useSpecificExperimentForDesktopBuild"/> is true.
         /// </summary>
         public Experiment desktopExperiment;
-        [Header("MTurk Options (if used)")]
         [TextArea(3, 3)]
         /// <summary>
         /// What message should be shown to the user when they complete the game as a Amazon Mechanical Turk user. \
@@ -51,7 +44,6 @@ namespace PlayUR
         /// </summary>
         public string mTurkCompletionMessage = "HiT Completed\nCode: 6226";
 
-        [Header("Editor-Only Testing")]
         /// <summary>
         /// For use in-editor only, this allows us to test the game with the Experiment defined in <see cref="experimentToTestInEditor"/>.
         /// </summary>
@@ -77,8 +69,6 @@ namespace PlayUR
         /// </summary>
         public string forceMTurkIDInEditor = null;
 
-
-        [Header("Prefabs and Assets")]
         /// <summary>
         /// The prefab to use that represents the highscore table. You can link this to the pre-made prefab in the PlayUR/HighScores folder, or create your own.
         /// </summary>
@@ -104,14 +94,10 @@ namespace PlayUR
         /// </summary>
         public Sprite mTurkLogo;
 
-
-        [Header("Logging")]
         /// <summary>
         /// The minimum log level to store in the PlayUR Platform. This is useful if you want to ignore certain log messages.
         /// </summary>
         public PlayURPlugin.LogLevel minimumLogLevelToStore = PlayURPlugin.LogLevel.Log;
-
-
 
         internal static PlayURSettings GetOrCreateSettings()
         {
