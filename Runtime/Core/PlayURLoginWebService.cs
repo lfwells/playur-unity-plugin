05c6eff0f38559c44a37554be67852c1e9dbe4a1
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2023-12-05 02:48:58 +1100

    Update PlayURLoginWebService.cs

--- Runtime/Core/PlayURLoginWebService.cs
+++ Runtime/Core/PlayURLoginWebService.cs
@@ -7,9 +7,6 @@ using System.Threading;
 using UnityEngine;
 using System.Linq;
 using System.Text;
-using System.IO;
-using System.Security.Cryptography;
-using System.Buffers.Binary;
 
 namespace PlayUR
 {
