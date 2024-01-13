cef4d5b516c298783cac34eadf27017cf9d043c3
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2024-01-14 02:50:00 +1100

    Add functionality for updatable actions

--- Runtime/Core/PlayURAnalytics.cs
+++ Runtime/Core/PlayURAnalytics.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,7 @@ namespace PlayUR
             public List<string> columnValues; 
         }
         [System.Serializable]
-        protected struct ActionParamsList //weidly to serialize to json, we need a struct to wrap the array
+        protected struct ActionParamsList //weirdly to serialize to json, we need a struct to wrap the array
         {
             public ActionParams[] actions;
         }
@@ -39,6 +40,25 @@ namespace PlayUR
         ///(note, original timestamp will be preserved) -- defaults to true. </param>
         /// <param name="columns">Optionally any custom-set up columns and their data. For complex data, use <see cref="BeginRecordAction(Action)"/> instead.</param>
         public void RecordAction(Action a, object extra = null, bool HTMLencode = false, bool debugOutput = false, bool waitForPendingActions = true, Dictionary<AnalyticsColumn, object> columns = null)
+        {
+            actionQueue.Enqueue(GenerateUploadableActionData(a,extra,HTMLencode,debugOutput,waitForPendingActions,columns));
+        }
+
+        IEnumerator RecordActionDirectly(ActionParams singleAction, Rest.ServerCallback callback)
+        {
+            yield return StartCoroutine(RecordActionDirectly(new ActionParamsList { actions = new ActionParams[] { singleAction } }, callback));
+        }
+        IEnumerator RecordActionDirectly(ActionParamsList actions, Rest.ServerCallback callback)
+        {
+            var form = Rest.GetWWWFormWithExperimentInfo();
+            form.Add("actions", JsonUtility.ToJson(actions));
+
+            while (!inSession) yield return new WaitForEndOfFrame();
+            form.Add("sessionID", sessionID.ToString());
+
+            yield return StartCoroutine(Rest.Post("UserAction", form, HTMLencode: false, debugOutput: true, callback: callback));
+        }
+        ActionParams GenerateUploadableActionData(Action a, object extra = null, bool HTMLencode = false, bool debugOutput = false, bool waitForPendingActions = true, Dictionary<AnalyticsColumn, object> columns = null)
         {
             var timestamp = GetMysqlFormatTime();
 
@@ -77,12 +97,9 @@ namespace PlayUR
                 }
             }
 
-            //check to see if the 
-
-            actionQueue.Enqueue(parameters);
-            
-            //StartCoroutine(RecordActionRoutine(a, timestamp, extra, HTMLencode, debugOutput, waitForPendingActions));
+            return parameters;
         }
+
         IEnumerator RecordActionRoutine()
         {
             while (true)
@@ -103,7 +120,6 @@ namespace PlayUR
             {
                 Log("Sending off "+actionQueue.Count+" actions in a batch");
 
-                var form = Rest.GetWWWFormWithExperimentInfo();
                 ActionParamsList actions = new ActionParamsList()
                 {
                     actions = new ActionParams[actionQueue.Count]
@@ -111,19 +127,9 @@ namespace PlayUR
                 actions.actions = actionQueue.ToArray();
                 actionQueue.Clear();
                 Log(JsonUtility.ToJson(actions));
-                
-                form.Add("actions", JsonUtility.ToJson(actions));
-                //form.Add("actionID", ((int)a).ToString());
-                //form.Add("timestamp", timestamp);
-                //form.Add("experimentID", configuration.experimentID.ToString());
-                //form.Add("experimentGroupID", configuration.experimentGroupID.ToString());
-                //if (extra != null)
-                    //form.Add("extra", extra.ToString());
-                if (inSession)
-                    form.Add("sessionID", sessionID.ToString());
 
                 //pendingActions = true;
-                yield return StartCoroutine(Rest.EnqueuePost("UserAction", form, HTMLencode: false, debugOutput: true));
+                yield return StartCoroutine(RecordActionDirectly(actions, callback: null));
             }
         }
         /// <summary>
@@ -162,7 +168,7 @@ namespace PlayUR
             /// </summary>
             public Action action;
 
-            Dictionary<AnalyticsColumn, object> columns = new Dictionary<AnalyticsColumn, object>();
+            internal Dictionary<AnalyticsColumn, object> columns = new Dictionary<AnalyticsColumn, object>();
 
             /// <summary>
             /// Upload the action to the server.
@@ -174,12 +180,7 @@ namespace PlayUR
             ///(note, original timestamp will be preserved) -- defaults to true. </param>
             public void Record(bool HTMLencode = false, bool debugOutput = false, bool waitForPendingActions = true)
             {
-                var extras = new List<string>();
-                foreach (var column in PlayURPlugin.instance.configuration.analyticsColumnsOrder)
-                {
-                    if (columns.ContainsKey(column) && columns[column] != null)
-                        extras.Add(columns[column].ToString());
-                }
+                var extras = GetExtras();
                 PlayURPlugin.instance.RecordAction(this.action, string.Join(",",extras), HTMLencode:HTMLencode, debugOutput:debugOutput, waitForPendingActions:waitForPendingActions, columns:columns);
             }
 
@@ -203,6 +204,208 @@ namespace PlayUR
             public void UpdateColumn(AnalyticsColumn column, object value)
             {
                 AddColumn(column, value);
+            }
+
+            /// <summary>
+            /// Converts the columns for this action into a list of strings in order (used internally)
+            /// </summary>
+            /// <returns></returns>
+            internal List<string> GetExtras()
+            {
+                var extras = new List<string>();
+                foreach (var column in PlayURPlugin.instance.configuration.analyticsColumnsOrder)
+                {
+                    if (columns.ContainsKey(column) && columns[column] != null)
+                        extras.Add(columns[column].ToString());
+                }
+                return extras;
+            }
+
+            internal string GetExtra(AnalyticsColumn column)
+            {
+                if (columns.ContainsKey(column) == false) return null;
+                return columns[column].ToString();
+            }
+        }
+
+        protected ActionParams GenerateUploadableActionData(InProgressAction a, bool HTMLencode = false, bool debugOutput = false, bool waitForPendingActions = true)
+        {
+            return PlayURPlugin.instance.GenerateUploadableActionData(a.action, string.Join(",", a.GetExtras()), HTMLencode: HTMLencode, debugOutput: debugOutput, waitForPendingActions: waitForPendingActions, columns: a.columns);
+        }
+        #endregion
+
+        #region Updatable Actions
+        IEnumerator UpdatableActionsRoutine()
+        {
+            while (true)
+            {
+                foreach (var a in updatableActions)
+                {
+                    yield return StartCoroutine(a.Process());
+                }
+                yield return new WaitForSecondsRealtime(1f);
+            }
+        }
+
+        List<IProcessAction> updatableActions = new();
+
+        public interface IProcessAction
+        {
+            public IEnumerator Process();   
+        }
+        /// <summary>
+        /// Class to wrap an analytics action that can be updated over time. Can be updated frequently, and is updated to the server at a regular interval.
+        /// </summary>
+        /// <typeparam name="T">The type of data to record in the action. Should be a primative.</typeparam>
+        public class UpdatableAction<T> : IProcessAction
+        {
+            protected InProgressAction action;
+
+            bool initialUploadComplete = false;
+            bool initialUploadStarted = false;
+            Dictionary<AnalyticsColumn, bool> dirty = new Dictionary<AnalyticsColumn, bool>();
+            float lastUpdate = -1f;
+            int userActionID = -1;
+
+            /// <summary>
+            /// Data will only be sent to server this number of seconds since the last upload has completed. However if data has not updated, no additional upload will take place.
+            /// </summary>
+            public float uploadIntervalSeconds = 5f;
+
+            /// <summary>
+            /// Create an action that can be updated over time.
+            /// </summary>
+            /// <param name="action">The analytics action id to tag this action with</param>
+            public UpdatableAction(Action action)
+            {
+                this.action = instance.BeginRecordAction(action);
+                instance.updatableActions.Add(this);
+
+                var uploadData = instance.GenerateUploadableActionData(this.action);
+                instance.StartCoroutine(instance.RecordActionDirectly(uploadData, (bool succ, JSONNode result) => {
+                    if (succ)
+                    {
+                        initialUploadComplete = true;
+                        userActionID = result["id"];
+                    }
+                }));
+            }
+            /// <summary>
+            /// Create an action that can be updated over time, and set the initial data.
+            /// </summary>
+            /// <param name="action">The analytics action id to tag this action with</param>
+            /// <param name="column">The column to set initial data for</param>
+            /// <param name="initialData">The initial data to put into the column</param>
+            public UpdatableAction(Action action, AnalyticsColumn column, T initialData)
+            {
+                this.action = instance.BeginRecordAction(action);
+                instance.updatableActions.Add(this);
+
+                var uploadData = instance.GenerateUploadableActionData(this.action);
+                instance.StartCoroutine(instance.RecordActionDirectly(uploadData, (bool succ, JSONNode result) => {
+                    if (succ)
+                    {
+                        initialUploadComplete = true;
+                        userActionID = result["id"];
+
+                        Set(column, initialData);
+                    }
+                }));
+
+            }
+
+            /// <summary>
+            /// Update this action's data for the given column
+            /// </summary>
+            /// <param name="column">The column to change data for</param>
+            /// <param name="data">The value to set the column to (overrides original data, creates the column if it didn't previoulsy exist)</param>
+            /// <returns></returns>
+            public InProgressAction Set(AnalyticsColumn column, T data)
+            { 
+                this.action.UpdateColumn(column, data);
+
+                if (this.dirty.ContainsKey(column) == false)
+                {
+                    this.dirty.Add(column, true);
+                }
+                this.dirty[column] = true;
+
+                return this.action;
+            }
+
+            /// <summary>
+            /// Run the actual upload. Used internally.
+            /// </summary>
+            /// <returns></returns>
+            public IEnumerator Process()
+            {
+
+                if (Time.realtimeSinceStartup - lastUpdate > uploadIntervalSeconds)
+                {
+                    if (initialUploadComplete == false)
+                    {
+                        yield break;
+                    }
+
+                    lastUpdate = Time.realtimeSinceStartup;
+                    foreach (var kvp in dirty.ToArray())
+                    {
+                        if (kvp.Value == true)//is dirty
+                        {
+                            var column = kvp.Key;
+                            dirty[column] = false;
+
+
+                            var form = Rest.GetWWWFormWithExperimentInfo();
+                            form.Add("userActionID", userActionID.ToString());
+                            form.Add("columnID", ((int)column).ToString());
+                            form.Add("value", action.GetExtra(column)?.ToString() ?? "");
+
+                            yield return instance.StartCoroutine(Rest.EnqueuePut("UserActionExtraColumn", -1, form, HTMLencode: false, debugOutput: false));
+                        }
+                    }
+                }
+
+            }
+            
+        }
+        /// <summary>
+        /// An action that can have data for a given column appended-to over time. Can be updated frequently, and is updated to the server at a regular interval.
+        /// </summary>
+        public class AppendableAction : UpdatableAction<string>
+        {
+            protected string currentDataValue = "";
+            protected AnalyticsColumn column;
+
+            /// <summary>
+            /// The current value stored in the column for this action. 
+            /// </summary>
+            public string Value { get { return currentDataValue; } }
+
+            /// <summary>
+            /// Create an action that can be appended-to over time.
+            /// </summary>
+            /// <param name="action">The analytics action id to tag this action with</param>
+            /// <param name="column">The column to use for storing the data</param>
+            public AppendableAction(Action action, AnalyticsColumn column) : base(action) { this.column = column; }
+
+            /// <summary>
+            /// Create an action that can be appended-to over time, and set the initial data.
+            /// </summary>
+            /// <param name="action">The analytics action id to tag this action with</param>
+            /// <param name="column">The column to use for storing the data</param>
+            /// <param name="initialData">The initial data to store in the column</param>
+            public AppendableAction(Action action, AnalyticsColumn column, string initialData) : base(action, column, initialData) { this.column = column; }
+
+            /// <summary>
+            /// Append data to the column for this action. Is not immediately uploaded to server, PlayUR handles this on a regular interval instead.
+            /// </summary>
+            /// <param name="data"></param>
+            /// <returns></returns>
+            public InProgressAction Append(string data)
+            {
+                this.currentDataValue += data;
+                return Set(this.column, this.currentDataValue);
             }
         }
         #endregion
