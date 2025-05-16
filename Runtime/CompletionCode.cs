701ee0d165ccb26f6d9d9ad2009201711f172b6d
Lindsay Wells <Lindsay.Wells@utas.edu.au>
2025-05-16 19:21:02 +1000

    compression changes

--- Runtime/CompletionCode.cs
+++ Runtime/CompletionCode.cs
@@ -149,10 +149,15 @@ namespace PlayUR
             yield break;
         }
 
-        static string Base64Encode(string plainText)
+        public static string Base64Encode(string plainText)
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
+        }
+        public static string Base64Decode(string plainText)
+        {
+            var bytes = System.Convert.FromBase64String(plainText);
+            return System.Text.Encoding.UTF8.GetString(bytes);
         }
 
         static class Rot13
